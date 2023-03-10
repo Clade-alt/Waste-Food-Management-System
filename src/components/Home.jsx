18cdc1218332c8cd16ef682bf5cc0a5873df9260
Chathumal Navanjana <chathumalnavanjana@gmail.com>
2023-03-10 22:04:48 +0530

    modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <section id="home">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8 mt-5">
+              <h1 className="display-4 fw-bolder mb-4 text-center text-white"> Lets Reduce Food Wastage</h1>
+              <p className="lead text-center fs-4 mb-5 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, quod illum non illo ad nostrum cumque quidem eum deleniti minima.</p>
+              <div className="buttons d-flex justify-content-center">
+                <div className="button btn btn-outline-light me-4 rounded-pill px-5 py-3 fs-5">Explore</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
             <nav class="navbar navbar-expand-lg shadow">
   <div class="container">
-    <a class=" navbar-brand fw-bolder fs-4 mx-auto" href="#">WASTE FOOD MANAGEMENT &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
+    <a class=" navbar-brand fw-bolder fs-4 mx-auto">WASTE FOOD MANAGEMENT &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
