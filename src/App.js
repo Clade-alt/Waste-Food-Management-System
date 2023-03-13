8a969d6cd5c549ea60f4f3ac913c6888568d87ae
Chathumal Navanjana <chathumalnavanjana@gmail.com>
2023-03-13 19:07:17 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Home/>
+      <AboutUs/>
+      <Services/>
     </>
   );
 }
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ContactUs = () => {
+    return (
+        <div>
+            <section id="contactus">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="fs-5 text-center mb-0">Our Services</h3>
+                            <h1 className="display-6 text-center mb-4">Our <b>Thoughtful</b> Services</h1>
+                            <hr className="w-25 mx-auto" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default ContactUs;
